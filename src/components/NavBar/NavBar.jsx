a3d2fe6e3ad0abd47a65131da9b3a8288b63b630
EmiFlores <emif94@gmail.com>
2021-11-30 23:06:54 -0300

    Se agrega searchbar al navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -34,6 +34,11 @@ export const NavBar = () => {
                             <a class="nav-link" href="#">Contacto</a>
                             </li>
                         </ul>
+
+                        <form class="d-flex">
+                            <input class="form-control me-2" type="search" placeholder="ej: Batman" aria-label="Search"/>
+                            <button class="btn btn-outline-success" type="submit">Buscar</button>
+                        </form>
                         
                         </div>
                     </div>
