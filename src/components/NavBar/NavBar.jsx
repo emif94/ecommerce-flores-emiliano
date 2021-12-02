be5d08a725bd65281139647ee83514000acfc74a
EmiFlores <emif94@gmail.com>
2021-12-02 20:17:41 -0300

    Se eliminaron archivos innecesarios, se cambió la Nav (de html a jsx), realizando las importaciones de los componentes correspondientes

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,51 +1,45 @@
 import React from 'react'
+import { Navbar, Container, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap'
 
 export const NavBar = () => {
     return (
         <div>
-            
-            <div>
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <div class="container-fluid">
-                        <a class="navbar-brand" href="#">Hero Collectibles</a>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Inicio</a>
-                            </li>
-                            <li class="nav-item">
-                            <a class="nav-link" href="#"></a>
-                            </li>
-                            
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Categorias
-                                </a>
-                                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                    <li><a class="dropdown-item" href="#">Lego´s</a></li>
-                                    <li><a class="dropdown-item" href="#">Figuras de Acción</a></li>
-                                    <li><a class="dropdown-item" href="#">Estatuas</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item">
-                            <a class="nav-link" href="#">Contacto</a>
-                            </li>
-                        </ul>
-
-                        <form class="d-flex">
-                            <input class="form-control me-2" type="search" placeholder="ej: Batman" aria-label="Search"/>
-                            <button class="btn btn-outline-success" type="submit">Buscar</button>
-                        </form>
-                        
-                        </div>
-                    </div>
-                </nav>
-            </div>
-
-            
+            <Navbar bg="light" expand="lg">
+            <Container fluid>
+              <Navbar.Brand href="#">Hero Collectibles</Navbar.Brand>
+              <Navbar.Toggle aria-controls="navbarScroll" />
+              <Navbar.Collapse id="navbarScroll">
+                <Nav
+                  className="me-auto my-2 my-lg-0"
+                  style={{ maxHeight: '100px' }}
+                  navbarScroll
+                >
+                  <Nav.Link href="#action1">Inicio</Nav.Link>
+                  <Nav.Link href="#action2">Nuevos</Nav.Link>
+                  <NavDropdown title="Categorías" id="navbarScrollingDropdown">
+                    <NavDropdown.Item href="#action3">Figuras de Acción</NavDropdown.Item>
+                    <NavDropdown.Item href="#action4">Lego´s</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action5">
+                      Something else here
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                  <Nav.Link href="#">
+                    Contacto
+                  </Nav.Link>
+                </Nav>
+                <Form className="d-flex">
+                  <FormControl
+                    type="search"
+                    placeholder="ej: Batman"
+                    className="me-2"
+                    aria-label="Search"
+                  />
+                  <Button variant="outline-success">Buscar</Button>
+                </Form>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>            
 
         </div>
     )
