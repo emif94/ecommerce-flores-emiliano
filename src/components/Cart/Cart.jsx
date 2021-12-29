96c6a5d05459d23b21307b25d965ffc7af0a7de4
EmiFlores <emif94@gmail.com>
2021-12-28 21:46:26 -0300

    Se genera el desglose del carrito, y la suma de los totales

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -6,7 +6,23 @@ import { useCartContext } from "../../context/CartContext"
 function Cart()  {
 
     const {cartList, borrarCarrito}= useCartContext()
+    let res = [0]
 
+    const total = [0]
+   // const sumarTotalCarrito = (cartList) =>{
+    //    for (const totalProd of cartList){
+    //       total.push(totalProd.precio) 
+    //    }
+    //    console.log(total)
+   // }
+function sumarTotalCarrito(){
+     res = cartList.reduce ((acc,item)=>{
+       return acc += (item.precio * item.cant)
+   },0)
+   console.log(res)
+
+}
+   
     return (
         <div>
             {cartList.map(prod=> 
@@ -18,8 +34,11 @@ function Cart()  {
 
                 
             </div>)}
-
+             
             <button onClick={borrarCarrito}>Eliminar productos del carrito</button>
+
+            {sumarTotalCarrito()}
+            <h2>Total : {`${res}`}</h2>
         </div>
     )
 }
