182b026741762c405a96d365cd4aa48718fafe24
EmiFlores <emif94@gmail.com>
2022-01-11 21:17:03 -0300

    Se agrega la generacion de orden. Da error y no genera la coleccion en firebase y da error : -Function addDoc() called with invalid data-

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,15 +1,46 @@
 //import {useContext} from 'react'
 //import { CartContext } from '../../context/CartContext'
-
+import {  addDoc, collection, doc, getFirestore, Timestamp } from "firebase/firestore"
 import { useCartContext } from "../../context/CartContext"
 import {Link} from 'react-router-dom'
+import { useState } from "react"
+
 
 function Cart()  {
-
+    const [idOrden, setIdOrden] = useState('')
     const {cartList, borrarCarrito, eliminarUnProducto}= useCartContext()
     let totalCarrito = [0]
     
     console.log(cartList)
+
+     const generarOrden = (e) =>{
+        e.preventDefault()    
+          
+        let orden = {}
+        orden.date = Timestamp.fromDate(new Date())
+       
+
+        orden.comprador = {nombre: 'Emiliano', tel: '6454', email: 'emif94@gmail.com'}
+        orden.total = sumarTotalCarrito();
+
+        orden.productos = cartList.map(cartItem => {
+            const id = cartItem.id;
+            const nombre = cartItem.nombre;
+            const precio = cartItem.precio * cartItem.cantidad;
+            
+            return {id, nombre, precio}  
+            
+        })
+        console.log (orden) 
+
+        // Generacion de orden
+         const db = getFirestore()
+        const ordenColeccion = collection(db, 'ordenes')
+        addDoc(ordenColeccion, orden)
+        .then(resp => setIdOrden(resp.id))
+        .catch(err => console.log(err))
+
+    }
 
 
 function sumarTotalCarrito(){
@@ -17,6 +48,7 @@ function sumarTotalCarrito(){
        return acc += (item.precio * item.cant)
    },0)
    console.log(totalCarrito)
+   console.log(cartList)
 
 }
 
@@ -38,6 +70,7 @@ function sumarTotalCarrito(){
 
                 
             </div>)}
+
              
             <button onClick={borrarCarrito}>Eliminar productos del carrito</button>
 
@@ -50,6 +83,20 @@ function sumarTotalCarrito(){
               </div> 
               :   
               `Total :${totalCarrito}` }</h2>
+
+              <div>
+            {idOrden.length !== 0 && idOrden}
+            {  cartList.map(prod=> <li>{prod.nombre}   {prod.cantidad}</li>) }
+            <form 
+                onSubmit={generarOrden} 
+                
+            >
+                
+                <button>Generar Orden</button>
+            </form>
+            <button onClick={borrarCarrito} >Vaciar Carrito</button>
+
+        </div>
               
         </div>
     )
